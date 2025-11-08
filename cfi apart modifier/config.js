cea36dc86c3317ef095bea4e2886728965a7df1d
bluvision30-netizen <bluvision30@gmail.com>
2025-11-08 13:09:16 +0100

    Add files via upload

---
+++ cfi apart modifier/config.js
@@ -0,0 +1,5 @@
+// config.js - À garder séparé
+const CONFIG = {
+    ONESIGNAL_APP_ID: "cafdfcde-aa21-414a-98de-f8e9c837738f",
+    ONESIGNAL_REST_API_KEY: "os_v2_org_ciumuqa3ibdvbl3tpbx4mhpdrfk6o2myxtuexjendtpjjpazicpg6pw2wvsal72ybq5shup7y4a3gyurqkgi2apt4g5qfn6qelsf6ri"
+};
