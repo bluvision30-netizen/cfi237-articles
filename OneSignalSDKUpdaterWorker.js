d6838e687ecd216f1700a790e5483904f61d7544
bluvision30-netizen <bluvision30@gmail.com>
2025-11-08 13:29:20 +0100

    Add files via upload

---
+++ OneSignalSDKUpdaterWorker.js
@@ -0,0 +1,2 @@
+// OneSignalSDKUpdaterWorker.js - Second fichier requis
+importScripts('https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js');
---
+++ OneSignalSDKWorker.js
@@ -0,0 +1,2 @@
+// OneSignalSDKWorker.js - Fichier requis par OneSignal
+importScripts('https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js');
